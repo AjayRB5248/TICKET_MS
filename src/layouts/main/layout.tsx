a518b9dc87e98cd913bf2debc514de04a26cb3f1
dibyamagar56 <dibya.magar@portpro.io>
2024-01-23 23:21:37 +0545

    Fix: Add All Imports

--- src/layouts/main/layout.tsx
+++ src/layouts/main/layout.tsx
@@ -11,33 +11,32 @@ import { mainFont } from "src/theme/typography";
 
 // plugin css
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 import "src/assets/frontend/css/all.min.css";
+import "src/assets/frontend/css/animate.css";
+import "src/assets/frontend/css/flaticon.css";
+import "src/assets/frontend/css/magnific-popup.css";
+import "src/assets/frontend/css/odometer.css";
+import "src/assets/frontend/css/owl.carousel.min.css";
+import "src/assets/frontend/css/owl.theme.default.min.css";
 import "src/assets/frontend/css/animatedheadline.css";
 
 // custom css
 import "src/assets/frontend/sass/main.scss";
 
-// import "src/assets/frontend/css/animate.css";
-// import "src/assets/frontend/css/flaticon.css";
-// import "src/assets/frontend/css/magnific-popup.css";
-// import "src/assets/frontend/css/odometer.css";
-// import "src/assets/frontend/css/owl.carousel.min.css";
-// import "src/assets/frontend/css/owl.theme.default.min.css";
-
 // js
-// import "src/assets/frontend/js/heandline.js";
-// import "src/assets/frontend/js/jquery-3.3.1.min.js";
-// import "src/assets/frontend/js/modernizr-3.6.0.min.js";
-// import "src/assets/frontend/js/plugins.js";
-// import "src/assets/frontend/js/isotope.pkgd.min.js";
-// import "src/assets/frontend/js/magnific-popup.min.js";
-// import "src/assets/frontend/js/owl.carousel.min.js";
-// import "src/assets/frontend/js/wow.min.js";
-// import "src/assets/frontend/js/countdown.min.js";
-// import "src/assets/frontend/js/odometer.min.js";
-// import "src/assets/frontend/js/viewport.jquery.js";
-// import "src/assets/frontend/js/main.js";
+import "src/assets/frontend/js/jquery-3.3.1.min.js";
+import "src/assets/frontend/js/modernizr-3.6.0.min.js";
+import "src/assets/frontend/js/plugins.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "src/assets/frontend/js/heandline.js";
+import "src/assets/frontend/js/isotope.pkgd.min.js";
+import "src/assets/frontend/js/magnific-popup.min.js";
+import "src/assets/frontend/js/owl.carousel.min.js";
+import "src/assets/frontend/js/wow.min.js";
+import "src/assets/frontend/js/countdown.min.js";
+import "src/assets/frontend/js/odometer.min.js";
+import "src/assets/frontend/js/viewport.jquery.js";
+import "src/assets/frontend/js/main.js";
 
 // components
 import Preloader from "../_common/preloader";
