a24830ea7b615f1b848b173823cf0fdbc3cb8538
dibyamagar56 <dibya.magar@portpro.io>
2024-01-25 22:12:43 +0545

    Fix: Banner Section

--- src/sections/home/home-banner.tsx
+++ src/sections/home/home-banner.tsx
@@ -1,5 +1,5 @@
 import BannerBg from "src/assets/frontend/images/banner/banner12.jpg";
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 
 const Banner = () => {
   return (
@@ -8,23 +8,14 @@ const Banner = () => {
       <div className="container">
         <div className="banner-content">
           <h1 className="title  cd-headline clip">
-          <span className="d-block">book your</span> tickets for {""}
+            <span className="d-block">book your</span> tickets for {""}
             <TypeAnimation
-      sequence={[
-        'Concerts',
-        1000, 
-        'Sports',
-        1000,
-        'Events',
-        1000,
-        'Movies',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '0.7em', display: 'inline-block',color:'green'}}
-      repeat={Infinity}
-    />
+              sequence={["Concerts", 1000, "Sports", 1000, "Events", 1000, "Movies", 1000]}
+              wrapper="span"
+              speed={1}
+              style={{ display: "inline-block", color: "#31d7a9" }}
+              repeat={Infinity}
+            />
           </h1>
           <p>Safe, secure, reliable ticketing.Your ticket to live entertainment!</p>
         </div>
