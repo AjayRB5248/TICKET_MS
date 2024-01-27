7f05307230b686d95aaa4c737d8051b6e05218ec
dibyamagar56 <dibya.magar@portpro.io>
2024-01-28 01:26:18 +0545

    Add: New Auth Provider Context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,9 @@ import MotionLazy from "src/components/animate/motion-lazy";
 import SnackbarProvider from "src/components/snackbar/snackbar-provider";
 import { SettingsProvider, SettingsDrawer } from "src/components/settings";
 // auth
-import { AuthProvider, AuthConsumer } from "src/auth/context/jwt";
+import { AuthConsumer } from "src/auth/context/jwt";
+import { AuthProvider } from "src/auth/context/user/auth-provider";
+
 // import { AuthProvider, AuthConsumer } from 'src/auth/context/auth0';
 // import { AuthProvider, AuthConsumer } from 'src/auth/context/amplify';
 // import { AuthProvider, AuthConsumer } from 'src/auth/context/firebase';
--- src/auth/hooks/use-auth-context.ts
+++ src/auth/hooks/use-auth-context.ts
@@ -1,8 +1,10 @@
-'use client';
+"use client";
 
-import { useContext } from 'react';
+import { useContext } from "react";
 //
-import { AuthContext } from '../context/jwt/auth-context';
+
+import { AuthContext } from "../context/user/auth-context";
+// import { AuthContext } from '../context/jwt/auth-context';
 // import { AuthContext } from '../context/auth0/auth-context';
 // import { AuthContext } from '../context/amplify/auth-context';
 // import { AuthContext } from '../context/firebase/auth-context';
@@ -11,9 +13,8 @@ import { AuthContext } from '../context/jwt/auth-context';
 
 export const useAuthContext = () => {
   const context = useContext(AuthContext);
-  console.log(context, "context-----")
 
-  if (!context) throw new Error('useAuthContext context must be use inside AuthProvider');
+  if (!context) throw new Error("useAuthContext context must be use inside AuthProvider");
 
   return context;
 };
