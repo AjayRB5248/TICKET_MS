e000724bf98d0a18016018c8ae3853478064c105
dibyamagar56 <dibya.magar@portpro.io>
2024-02-04 01:37:08 +0545

    Integrate Widget Checkboxes

--- src/sections/events-list/view/widget-checkbox.tsx
+++ src/sections/events-list/view/widget-checkbox.tsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEventsContext } from "src/context/EventsContextProvider";
+import { EVENT_CATEGORIES } from "src/sections/tour/utils";
+import EventsService from "src/services/events";
 
-const categories = [
-  "all",
-  "Screening",
-  "meetings",
-  "PerhtmlFormances",
-  "workshops",
-  "Exhibitions",
-  "music shows",
-  "comedy shows",
-  "Award shows",
-];
+const categories = EVENT_CATEGORIES.map((eachEventCategory: any) => eachEventCategory);
 
 const WidgetCheckbox: React.FC = () => {
+  const { setEvents, setFilters } = useEventsContext();
+  const [checkedCategories, setCheckedCategories] = useState<string[]>([]);
+
+  const handleCheckboxChange = async (category: string) => {
+    if (checkedCategories.includes(category)) {
+      setCheckedCategories(checkedCategories.filter((c) => c !== category));
+    } else {
+      setCheckedCategories([...checkedCategories, category]);
+    }
+
+    console.log(category, checkedCategories, "category -- checkedCategories");
+
+    const queryData = {
+      eventName: "",
+      city: "",
+      eventDate: "",
+      venueName: "",
+      category,
+    };
+    setFilters(queryData);
+    
+    const events = await EventsService.list(queryData)
+      .then((res) => res.data?.events)
+      .catch((err) => {
+        console.log(err, "err===");
+      });
+
+    setEvents(events);
+  };
+
   return (
     <div className="widget-1 widget-check">
       <div className="widget-header">
@@ -24,10 +47,16 @@ const WidgetCheckbox: React.FC = () => {
       <div className="widget-1-body">
         <h6 className="subtitle">categories</h6>
         <div className="check-area">
-          {categories.map((category, index) => (
+          {categories.map((eachCategory, index) => (
             <div className="form-group" key={index}>
-              <input type="checkbox" name="lang" id={`cat${index + 1}`} />
-              <label htmlFor={`cat${index + 1}`}>{category}</label>
+              <input
+                type="checkbox"
+                name="lang"
+                id={eachCategory.value}
+                checked={checkedCategories.includes(eachCategory.value)}
+                onChange={() => handleCheckboxChange(eachCategory.value)}
+              />
+              <label htmlFor={eachCategory.value}>{eachCategory.label}</label>
             </div>
           ))}
         </div>
