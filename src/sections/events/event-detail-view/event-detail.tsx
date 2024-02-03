6a3941b9ddc449364d219bd4eeac53948b99289b
dibyamagar56 <dibya.magar@portpro.io>
2024-02-04 03:22:01 +0545

    Add Event Detail Banner

--- src/sections/events/event-detail-view/event-detail.tsx
+++ src/sections/events/event-detail-view/event-detail.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { useFetchEvents } from "src/api/events";
 import { usePathname } from "src/routes/hook/use-pathname";
+import EventDetailBanner from "./event-detail-banner";
+import EventsService from "src/services/events";
+import { useEventsContext } from "src/context/EventsContextProvider";
 
 const EventDetail = () => {
   const pathname = usePathname();
 
-  const { events } = useFetchEvents();
-
   const [slug, setSlug] = useState<string | null>(null);
+
+  const [eventDetail, setEventDetail] = useState<any>();
 
   useEffect(() => {
     if (pathname) {
@@ -19,14 +22,34 @@ const EventDetail = () => {
   }, [pathname]);
 
   useEffect(() => {
-    console.log(events, "events!!!!!")
-    
-    const eventDetail = events?.find((eachEvent: any) => eachEvent.slug === slug);
-    console.log(eventDetail, "eventDetail")
+    if (slug) {
+      const fetchEventData = async () => {
+        try {
+          const eventData = await EventsService.list().then((res) => res.data?.events);
+
+          const foundEventDetail = eventData?.find((eachEvent: any) => eachEvent.slug === slug);
+          console.log(foundEventDetail, "foundEventDetail");
+
+          if (foundEventDetail) setEventDetail(foundEventDetail);
+        } catch (error) {
+          console.error("Error fetching event data:", error);
+        }
+      };
+
+      fetchEventData();
+    }
   }, [slug]);
 
   console.log(slug, "slug in detail page====");
-  return <div>Detail Page</div>;
+
+  console.log(eventDetail, "eventDetail===")
+  const primaryBannerImg = eventDetail?.eventImages?.find((eachEventImg: any) => eachEventImg.isPrimary).imageurl;
+
+  return (
+    <>
+      <EventDetailBanner bannerImg={primaryBannerImg} />
+    </>
+  );
 };
 
 export default EventDetail;
